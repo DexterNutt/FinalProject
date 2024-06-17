8def347576e798d32233d759baf96ad1d87f7e85
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-06-17 21:38:05 +0200

    Data goes as second parameter in axios post call !!! bug resolved

--- MentorToken/src/api/authApi.jsx
+++ MentorToken/src/api/authApi.jsx
@@ -5,15 +5,20 @@ export const LogInUser = async (email, password) => {
   const header = {
     "Content-Type": "application/json",
     Accept: "application/json",
+    "Access-Control-Allow-Headers":
+      "Origin, X-Requested-With, Content-Type, Accept",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, PUT, POST",
+    // "Content-Type": "application/json-patch+json",
   };
   const data = {
     email: email,
     password: password,
   };
+
   try {
-    const json = await axios.post(`${api.localRoute}/login`, {
+    const json = await axios.post(`${api.localRoute}/api/v1/auth/login`, data, {
       headers: header,
-      data: data,
     });
     return await Promise.resolve(json);
   } catch (err) {
@@ -28,7 +33,7 @@ export const registerUser = async (email, password) => {
   };
   const data = { username: email, password: password };
   try {
-    const json = await axios.post(`${api.localRoute}/register`, {
+    const json = await axios.post(`${api.localRoute}/api/v1/auth/register`, {
       headers: header,
       body: data,
     });
