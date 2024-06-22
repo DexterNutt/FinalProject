0499a96db4f1bf1fabbda4e2b3e2b51fc9dd3d12
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-06-22 15:11:46 +0200

    Implement overflow hidden on Login and Register

--- MentorToken/src/pages/Login.jsx
+++ MentorToken/src/pages/Login.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { RocketComp } from "../components/rocket/RocketComp";
 import { LoginForm } from "../components/auth/loginForm/LoginForm";
 import styles from "./Login.module.css";
 
 export const Login = () => {
+  useEffect(() => {
+    // Add the overflow hidden class to the body
+    document.body.classList.add(styles.noOverflow);
+
+    // Clean up the effect by removing the class when the component unmounts
+    return () => {
+      document.body.classList.remove(styles.noOverflow);
+    };
+  }, []);
+
   return (
     <div className={styles.wrapper}>
       <RocketComp className={styles.left} />
--- MentorToken/src/pages/Register.jsx
+++ MentorToken/src/pages/Register.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { RocketComp } from "../components/rocket/RocketComp";
 import { RegisterForm } from "../components/auth/registerForm/RegisterForm";
 import styles from "./Register.module.css";
 
 export const Register = () => {
+  useEffect(() => {
+    // Add the overflow hidden class to the body
+    document.body.classList.add(styles.noOverflow);
+
+    // Clean up the effect by removing the class when the component unmounts
+    return () => {
+      document.body.classList.remove(styles.noOverflow);
+    };
+  }, []);
+
   return (
     <div className={styles.wrapper}>
       <RocketComp className={styles.left} />
