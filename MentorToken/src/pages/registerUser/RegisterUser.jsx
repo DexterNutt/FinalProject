903164e4cdc1afd9c8e0f7369251d0340baa1779
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-06-23 13:37:49 +0200

    setup register User comp

---
+++ MentorToken/src/pages/registerUser/RegisterUser.jsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from "react";
+import { RegisterStartup } from "../../components/register/registerStartup/RegisterStartup";
+import { RegisterMentor } from "../../components/register/registerMentor/RegisterMentor";
+import { RocketComp } from "../../components/rocket/RocketComp";
+import styles from "./RegisterUser.module.css";
+
+export const RegisterUser = () => {
+  useEffect(() => {
+    document.body.classList.add(styles.noOverflow);
+    return () => {
+      document.body.classList.remove(styles.noOverflow);
+    };
+  }, []);
+
+  return (
+    <div className={styles.wrapper}>
+      <RocketComp className={styles.rocket} />
+      <RegisterStartup className={styles.right} />
+      {/* <RegisterMentor className={styles.right} /> */}
+    </div>
+  );
+};
