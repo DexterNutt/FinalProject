6aadc48d791cb55642d9f0d3503d7d4137f57035
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-06-24 13:29:24 +0200

    Rewrite register logic function

--- MentorToken/src/api/authApi.jsx
+++ MentorToken/src/api/authApi.jsx
@@ -5,11 +5,10 @@ export const LogInUser = async (email, password) => {
   const header = {
     "Content-Type": "application/json",
     Accept: "application/json",
-    "Access-Control-Allow-Headers":
-      "Origin, X-Requested-With, Content-Type, Accept",
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET, PUT, POST",
-    // "Content-Type": "application/json-patch+json",
+    // "Access-Control-Allow-Headers":
+    // "Origin, X-Requested-With, Content-Type, Accept",
+    // "Access-Control-Allow-Origin": "*",
+    // "Access-Control-Allow-Methods": "GET, PUT, POST",
   };
   const data = {
     email: email,
@@ -26,17 +25,42 @@ export const LogInUser = async (email, password) => {
   }
 };
 
-export const registerUser = async (email, password) => {
+export const registerUser = async (
+  email,
+  password,
+  role,
+  mentorName,
+  startupName,
+  representative,
+  address
+) => {
   const header = {
     "Content-Type": "application/json",
     Accept: "application/json",
   };
-  const data = { username: email, password: password };
+
+  const data = {
+    email: email,
+    password: password,
+    role: role,
+  };
+
+  if (role === "mentor") {
+    data.mentorName = mentorName;
+  } else if (role === "startup") {
+    data.startupName = startupName;
+    data.representative = representative;
+    data.address = address;
+  }
+
   try {
-    const json = await axios.post(`${api.localRoute}/api/v1/auth/register`, {
-      headers: header,
-      body: data,
-    });
+    const json = await axios.post(
+      `${api.localRoute}/api/v1/auth/register`,
+      data,
+      {
+        headers: header,
+      }
+    );
     return await Promise.resolve(json);
   } catch (err) {
     return await Promise.reject(err);
