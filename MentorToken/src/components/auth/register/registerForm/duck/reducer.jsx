6359a246f94f23d692bfd162a57105654816598f
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-06-24 15:44:53 +0200

    update the redux store

--- MentorToken/src/components/auth/register/registerForm/duck/reducer.jsx
+++ MentorToken/src/components/auth/register/registerForm/duck/reducer.jsx
@@ -1,9 +1,22 @@
-import { REGISTER_SUCCESS, REGISTER_FAILED } from "./constants";
+import {
+  REGISTER_SUCCESS,
+  REGISTER_FAILED,
+  UPDATE_EMAIL,
+  UPDATE_ROLE,
+  UPDATE_PASSWORD,
+  UPDATE_MENTOR_DATA,
+  UPDATE_STARTUP_DATA,
+} from "./constants";
 import { setUserStorage } from "../../../../../config/StorageFunctions";
 
 const initialState = {
   token: null,
   user: null,
+  email: "",
+  password: "",
+  role: "startup",
+  mentorData: {},
+  startupData: {},
 };
 
 export default function reducer(state = initialState, action) {
@@ -22,9 +35,18 @@ export default function reducer(state = initialState, action) {
         ...state,
         err: action.payload,
       };
-    default:
-      return {
-        ...state,
-      };
+    case UPDATE_EMAIL:
+      return { ...state, email: action.payload };
+    case UPDATE_PASSWORD:
+      return { ...state, password: action.payload };
+    case UPDATE_ROLE:
+      return { ...state, role: action.payload };
+    case UPDATE_MENTOR_DATA:
+      return { ...state, mentorData: action.payload };
+    case UPDATE_STARTUP_DATA:
+      return { ...state, startupData: action.payload };
   }
+  return {
+    ...state,
+  };
 }
--- MentorToken/src/store.jsx
+++ MentorToken/src/store.jsx
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import logger from "redux-logger";
 import { authReducer } from "./components/auth/loginForm/duck";
+import { registerReducer } from "./components/auth/register/registerForm/duck";
 
 const reducer = {
   authReducer: authReducer,
+  registerReducer: registerReducer,
 };
 
 export default configureStore({
