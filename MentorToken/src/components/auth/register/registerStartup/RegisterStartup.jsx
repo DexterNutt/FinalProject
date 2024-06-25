1a866859bda4de47ab882435b91d0ee2a3dc8010
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-06-25 12:36:42 +0200

    replace image with vector and border for startup icon

--- MentorToken/src/components/auth/register/registerStartup/RegisterStartup.jsx
+++ MentorToken/src/components/auth/register/registerStartup/RegisterStartup.jsx
@@ -39,118 +39,114 @@ export const RegisterStartup = ({ onNext }) => {
     });
   };
   return (
-    <Box className={styles.registerContainer}>
-      <Box className={styles.registerRight}>
-        <Box className={styles.rightContainer}>
-          <Box className={styles.registerRightTop}>
-            <img className={styles.logo} src="/logo.svg" alt="logo" />
-            <h2>SETUP STARTUP ACCOUNT</h2>
-          </Box>
+    <Box className={styles.registerRight}>
+      <Box className={styles.registerRightTop}>
+        <img className={styles.logo} src="/logo.svg" alt="logo" />
+        <h2>SETUP STARTUP ACCOUNT</h2>
+      </Box>
 
-          <div className={styles.imageContainer}>
-            <img
-              className={styles.briefcase}
-              src="/startup-img.webp"
-              alt="briefcase"
-            />
-            <img className={styles.camera} src="/photo-img.webp" alt="camera" />
+      <div className={styles.photoContainer}>
+        <div className={styles.workPhoto}>
+          <img src="/work.png" alt="work photo" />
+          <div className={styles.camera}>
+            <img src="/photo-img.webp" alt="camera" />
           </div>
+        </div>
+      </div>
 
-          <Box
-            component="form"
-            noValidate
-            onSubmit={handleSubmit}
-            className={styles.form}
-          >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="Startup Name"
-              label="Startup Name"
-              name="Startup Name"
-              placeholder="My Startup Name"
-              value={startupName}
-              onChange={handleNameChange}
-              autoFocus
-              sx={inputFieldStyles}
-            />
+      <Box
+        component="form"
+        noValidate
+        onSubmit={handleSubmit}
+        className={styles.form}
+      >
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          id="Startup Name"
+          label="Startup Name"
+          name="Startup Name"
+          placeholder="My Startup Name"
+          value={startupName}
+          onChange={handleNameChange}
+          autoFocus
+          sx={inputFieldStyles}
+        />
 
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="Legal Representative"
-              label="Legal Representative"
-              type="representative"
-              id="legalRepresentative"
-              placeholder="Name and Surname"
-              value={startupRepresentative}
-              onChange={handleRepChange}
-              sx={inputFieldStyles}
-            />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="Legal Representative"
+          label="Legal Representative"
+          type="representative"
+          id="legalRepresentative"
+          placeholder="Name and Surname"
+          value={startupRepresentative}
+          onChange={handleRepChange}
+          sx={inputFieldStyles}
+        />
 
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="address"
-              label="Registered Business Address"
-              placeholder="Registered Business Address"
-              type="address"
-              id="startupAddress"
-              value={startupAddress}
-              onChange={handleAddressChange}
-              sx={inputFieldStyles}
-            />
+        <TextField
+          margin="normal"
+          required
+          fullWidth
+          name="address"
+          label="Registered Business Address"
+          placeholder="Registered Business Address"
+          type="address"
+          id="startupAddress"
+          value={startupAddress}
+          onChange={handleAddressChange}
+          sx={inputFieldStyles}
+        />
 
-            <TextField
-              margin="normal"
-              fullWidth
-              name="inviteMentor"
-              label="Invite Mentors via email"
-              placeholder="Enter email address to invite mentor"
-              type="inviteMentor"
-              id="inviteMentor"
-              value={inviteMentor}
-              onChange={handleInviteMentorChange}
-              sx={inputFieldStyles}
-            />
+        <TextField
+          margin="normal"
+          fullWidth
+          name="inviteMentor"
+          label="Invite Mentors via email"
+          placeholder="Enter email address to invite mentor"
+          type="inviteMentor"
+          id="inviteMentor"
+          value={inviteMentor}
+          onChange={handleInviteMentorChange}
+          sx={inputFieldStyles}
+        />
 
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              className={styles.registerButton}
-              sx={formButtonStyles}
-            >
-              Register
-            </Button>
-            <div className={styles.termsAndConditions}>
-              <div className={styles.text}>
-                <FormControlLabel
-                  control={
-                    <Checkbox
-                      className={styles.checkbox}
-                      sx={{
-                        color: "#696cff",
-                        "&.Mui-checked": {
-                          color: "#696cff",
-                        },
-                      }}
-                    />
-                  }
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          className={styles.registerButton}
+          sx={formButtonStyles}
+        >
+          Register
+        </Button>
+        <div className={styles.termsAndConditions}>
+          <div className={styles.text}>
+            <FormControlLabel
+              control={
+                <Checkbox
+                  className={styles.checkbox}
+                  sx={{
+                    color: "#696cff",
+                    "&.Mui-checked": {
+                      color: "#696cff",
+                    },
+                  }}
                 />
-                <span>
-                  By signing up to create an account I accept Company’s{" "}
-                  <a href="#" className={styles.loginLinkText}>
-                    Terms of use & Privacy Policy.
-                  </a>
-                </span>
-              </div>
-            </div>
-          </Box>
-        </Box>
+              }
+            />
+            <span>
+              By signing up to create an account I accept Company’s{" "}
+              <a href="#" className={styles.loginLinkText}>
+                Terms of use & Privacy Policy.
+              </a>
+            </span>
+          </div>
+        </div>
       </Box>
     </Box>
   );
