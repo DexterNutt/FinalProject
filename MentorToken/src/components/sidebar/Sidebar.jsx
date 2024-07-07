2df644f9a084411651736ed4d690a600d5cda6fd
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-07-07 16:39:43 +0200

    add logic to have activeItem styles

--- MentorToken/src/components/sidebar/Sidebar.jsx
+++ MentorToken/src/components/sidebar/Sidebar.jsx
@@ -2,14 +2,26 @@ import React, { useState } from "react";
 import styles from "./Sidebar.module.css";
 
 export const Sidebar = () => {
+  const [activeItem, setActiveItem] = useState(0);
+
+  const handleItemClick = (item) => {
+    setActiveItem(item === activeItem ? activeItem : item);
+  };
+
   return (
-    <div className={styles.Sidebar}>
+    <div className={styles.sidebar}>
       <div className={styles.logo}>
         <img src="/logo.svg" alt="logo" />
         <p>Mentor Token</p>
       </div>
+      <div className={styles.hideMenu}>
+        <img src="/vectors/dashboard/hideIcon.svg" alt="arrow left" />
+      </div>
       <div className={styles.LinksContainer}>
-        <div className={styles.dashboard}>
+        <div
+          className={`${styles.link} ${activeItem === 0 ? styles.active : ""}`}
+          onClick={() => handleItemClick(0)}
+        >
           <div className={styles.icon}>
             <img src="/vectors/dashboard/category.svg" alt="category icon" />
           </div>
@@ -17,7 +29,10 @@ export const Sidebar = () => {
             <span>Dashboard</span>
           </div>
         </div>
-        <div className={styles.myStats}>
+        <div
+          className={`${styles.link} ${activeItem === 1 ? styles.active : ""}`}
+          onClick={() => handleItemClick(1)}
+        >
           <div className={styles.icon}>
             <img src="/vectors/dashboard/trello.svg" alt="trello icon" />
           </div>
@@ -25,7 +40,10 @@ export const Sidebar = () => {
             <span>My Stats</span>
           </div>
         </div>
-        <div className={styles.jobFeed}>
+        <div
+          className={`${styles.link} ${activeItem === 2 ? styles.active : ""}`}
+          onClick={() => handleItemClick(2)}
+        >
           <div className={styles.icon}>
             <img src="/vectors/dashboard/disc.svg" alt="disc icon" />
           </div>
@@ -33,12 +51,13 @@ export const Sidebar = () => {
             <span>Job Feed</span>
           </div>
         </div>
-
-        <div className={styles.logout}>
-          <div className={styles.icon}></div>
-          <div className={styles.text}>
-            <span>Logout</span>
-          </div>
+      </div>
+      <div className={styles.logout}>
+        <div className={styles.icon}>
+          <img src="/vectors/dashboard/logout.svg" alt="logout arrow" />
+        </div>
+        <div className={styles.text}>
+          <span>Logout</span>
         </div>
       </div>
     </div>
