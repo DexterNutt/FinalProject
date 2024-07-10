66205e3ee2330619c393979ab15096ff5847241e
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-07-10 14:40:08 +0200

    Render the Jobs component on Test

--- MentorToken/src/pages/TestPage.jsx
+++ MentorToken/src/pages/TestPage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { Sidebar } from "../components/sidebar/Sidebar";
+import { Jobs } from "../components/jobs/Jobs";
 
 export const TestPage = () => {
   return (
     <>
-      <Sidebar />
+      <Jobs />
     </>
   );
 };
