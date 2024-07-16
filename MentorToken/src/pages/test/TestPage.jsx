17b48d5c2e89a60b762450f9d67f6b776ad3f148
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-07-16 15:30:52 +0200

    reimport jobs

--- MentorToken/src/pages/test/TestPage.jsx
+++ MentorToken/src/pages/test/TestPage.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import styles from "./TestPage.module.css";
-import { Sidebar } from "../../components/sidebar/Sidebar";
 import { Jobs } from "../../components/jobs/Jobs";
 
 export const TestPage = () => {
   return (
     <div className={styles.wrapper}>
-      <Sidebar />
       <Jobs />
     </div>
   );
