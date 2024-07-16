7b1103e9a61edaf6b4d6c4c3c77877b62c840430
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-07-16 15:13:24 +0200

    Add User Profile component

---
+++ MentorToken/src/components/userProfile/UserProfile.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./UserProfile.module.css";
+
+export const UserProfile = () => {
+  return (
+    <div className={styles.userProfile}>
+      <img
+        src="/avatars/7.webp"
+        alt="User Profile"
+        className={styles.profileImage}
+      />
+      <span className={styles.userName}>Kirra Press</span>
+      <span className={styles.userRole}>Mentor</span>
+    </div>
+  );
+};
