75fc971d4067c7b38ee25bb873c2dac547fd8d26
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-08-08 12:26:34 +0200

    adjust add new button css

--- MentorToken/src/components/startupDashboard/feed/jobList/JobList.jsx
+++ MentorToken/src/components/startupDashboard/feed/jobList/JobList.jsx
@@ -4,13 +4,17 @@ import styles from "./JobList.module.css";
 import { JobCard } from "../jobCard/JobCard";
 
 export const JobList = () => {
+  const handleViewMore = () => {
+    setIsModalOpen(true);
+  };
+
   return (
     <div className={styles.jobList}>
       <div className={styles.title}>
         <p>Your Startup Jobs</p>
-        <button className={styles.creteJobButton}>
+        <button className={styles.creteJobButton} onClick={handleViewMore}>
           <img src="/vectors/dashboard/plus.svg" alt="plus icon" />
-          Create Job
+          Create New Job
         </button>
       </div>
       {data.jobs.map((job, index) => (
