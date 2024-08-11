910cd1f91afb42523c32ec201e77d420ef0e035e
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-08-11 15:22:44 +0200

    rewrite data import

--- MentorToken/src/components/startupDashboard/mentors/MentorList/MentorList.jsx
+++ MentorToken/src/components/startupDashboard/mentors/MentorList/MentorList.jsx
@@ -5,12 +5,12 @@ import { MentorCard } from "../mentorCard/MentorCard";
 
 export const MentorList = () => {
   const bestMentorRating = Math.max(
-    ...data.companyMentors.map((mentor) => mentor.average)
+    ...data.mentors.map((mentor) => mentor.average)
   );
 
   return (
     <div className={styles.mentorList}>
-      {data.companyMentors.map((mentor, index) => (
+      {data.mentors.map((mentor, index) => (
         <MentorCard
           key={index}
           mentor={mentor}
