062febe99405e3d1ebb1f6cdca17f33d6943b646
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-08-11 15:22:19 +0200

    reformat mentorProfile comp for more readability and correct import from data

--- MentorToken/src/components/mentorDashboard/stats/mentorStats/mentorProfile/MentorProfile.jsx
+++ MentorToken/src/components/mentorDashboard/stats/mentorStats/mentorProfile/MentorProfile.jsx
@@ -3,41 +3,43 @@ import styles from "./MentorProfile.module.css";
 
 export const MentorProfile = ({ mentorData }) => {
   return (
-    <>
-      <div className={styles.profileContainer}>
-        <div className={styles.profile}>
-          <div className={styles.picContainer}>
-            <img
-              src={mentorData.picture}
-              alt="mentor profile picture"
-              className={styles.profileImage}
-            />
-            <img
-              src="/vectors/verified-mark.svg"
-              alt="green verified checkmark "
-              className={styles.verifiedCheck}
-            />
-          </div>
-          <div className={styles.mentorName}>
-            <p>{mentorData.name}</p>
-            <img
-              src="/vectors/socials/radix-icons_linkedin-logo.svg"
-              alt="linkedIn logo "
-            />
-          </div>
-          <div className={styles.mentorTitle}>
-            <p>{mentorData.title}</p>
-          </div>
-          <div className={styles.mentorEmail}>
-            <img src="/vectors/dashboard/mail.svg" alt="mail icon" />
-            <p>{mentorData.email}</p>
-          </div>
-          <div className={styles.mentorPhone}>
-            <img src="/vectors/dashboard/phone.svg" alt="phone icon" />
-            <p>{mentorData.phone}</p>
-          </div>
+    <div className={styles.profileContainer}>
+      <div className={styles.profile}>
+        <div className={styles.picContainer}>
+          <img
+            src={mentorData.imageUrl}
+            alt="mentor profile"
+            className={styles.profileImage}
+          />
+          <img
+            src="/vectors/verified-mark.svg"
+            alt="verified checkmark"
+            className={styles.verifiedCheck}
+          />
+        </div>
+
+        <div className={styles.mentorName}>
+          <p>{mentorData.name}</p>
+          <img
+            src="/vectors/socials/radix-icons_linkedin-logo.svg"
+            alt="LinkedIn logo"
+          />
+        </div>
+
+        <div className={styles.mentorTitle}>
+          <p>{mentorData.title}</p>
+        </div>
+
+        <div className={styles.mentorEmail}>
+          <img src="/vectors/dashboard/mail.svg" alt="mail icon" />
+          <p>{mentorData.email}</p>
+        </div>
+
+        <div className={styles.mentorPhone}>
+          <img src="/vectors/dashboard/phone.svg" alt="phone icon" />
+          <p>{mentorData.phone}</p>
         </div>
       </div>
-    </>
+    </div>
   );
 };
