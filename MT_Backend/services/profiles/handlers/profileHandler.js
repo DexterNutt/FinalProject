083ea92f433a1ab1ac9355abcf85d01d0c132efa
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-09-03 12:51:18 +0200

    set correct projections for user query

--- MT_Backend/services/profiles/handlers/profileHandler.js
+++ MT_Backend/services/profiles/handlers/profileHandler.js
@@ -1,15 +1,12 @@
 const User = require("../../../pkg/users/userSchema");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
-// const mongo = require("mongodb");
-// const jwtDecode = require("");
 
 dotenv.config({ path: `${__dirname}/../../../pkg/config/config.env` });
 
 exports.findUser = async (req, res) => {
   try {
     const authToken = req.headers.authorization;
-    console.log(req.headers.authorization);
 
     if (authToken == null) {
       return res
@@ -19,15 +16,16 @@ exports.findUser = async (req, res) => {
 
     // STEP 1: DECODE
     const token = authToken.split(" ")[1];
-    console.log(token);
-
-    const decodedToken = jwt.verify(token, process.env.JWT_SECRET); // Decode the token
+    const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
 
     // STEP 2: EXTRACT ID
     const userId = decodedToken.id;
 
-    // STEP 3: FETCH IN DATABASE BY ID WITH PROJECTIONS ( NAME; SKILLS...; JUST THE THINGS I NEED )
-    const userData = await User.findById(userId, "name");
+    // STEP 3: FETCH IN DATABASE BY ID WITH PROJECTIONS
+    const userData = await User.findById(
+      userId,
+      "email phone description mentorName skills acceptedJobs"
+    );
 
     // STEP 4: SEND DATA TO FE
     if (!userData) {
