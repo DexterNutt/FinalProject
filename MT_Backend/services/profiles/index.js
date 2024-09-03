db6c17e7d3ebb98ab517f7f7e4547c8c7c98397b
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-09-03 10:21:39 +0200

    BUG RESOLVED - connect to the database!

--- MT_Backend/services/profiles/index.js
+++ MT_Backend/services/profiles/index.js
@@ -1,14 +1,16 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const users = require("./handlers/profileHandler");
+const db = require("./../../pkg/db/index");
 
 dotenv.config({ path: `${__dirname}/../../pkg/config/config.env` });
 
 const app = express();
 
+db.init();
 app.use(express.json());
 
-app.get("/api/v1/dashboard/mentor/:id", users.findUser);
+app.get("/api/v1/dashboard/mentor", users.findUser);
 
 app.listen(process.env.PORT_USERS, (err) => {
   if (err) {
