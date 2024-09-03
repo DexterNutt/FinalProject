090c675b50c9a0f5817bc339f01a15f8c798d4ad
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-09-03 10:18:19 +0200

    change usersApi to fetch user by token

--- MentorToken/src/api/usersApi.jsx
+++ MentorToken/src/api/usersApi.jsx
@@ -2,23 +2,25 @@ import axios from "axios";
 import { api } from "../config/properties";
 import { getToken } from "../config/StorageFunctions";
 
-export const fetchUser = async (userId) => {
+export const fetchUser = async () => {
   try {
     const token = getToken();
 
+    // REMEMBER: AXIOS USES 2 PARAMS THE URL AND CONFIG I.E. THE HEADERS IN THIS CASE
     const response = await axios.get(
-      `${api.localRoute}/api/v1/dashboard/mentor/${userId}`,
-      data,
+      `${api.localRoute}/api/v1/dashboard/mentor/`,
       {
         headers: {
           Authorization: `Bearer ${token}`,
+          Accept: "application/json",
+          "Content-Type": "application/json",
         },
       }
     );
 
     return response.data;
   } catch (err) {
-    console.log("Error fetching user data:", err);
+    console.log("Error fetching user data:", err.message);
     throw err;
   }
 };
