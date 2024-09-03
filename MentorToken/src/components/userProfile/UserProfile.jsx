e9d1cfbe0741d3e1aa42a4b55f8a979ce6d98f14
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-09-03 15:34:20 +0200

    Refactor to use data from Backend

--- MentorToken/src/components/userProfile/UserProfile.jsx
+++ MentorToken/src/components/userProfile/UserProfile.jsx
@@ -1,18 +1,32 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import styles from "./UserProfile.module.css";
 
 export const UserProfile = () => {
+  const { userData } = useSelector((state) => state.mentorDashboard);
+  console.log(userData);
+
   return (
     <div className={styles.userProfile}>
-      <img
-        src="/avatars/7.webp"
-        alt="User Profile"
-        className={styles.profileImage}
-      />
-      <div className={styles.userInfo}>
-        <span className={styles.userName}>Kirra Press</span>
-        <span className={styles.userRole}>Mentor</span>
-      </div>
+      {userData ? (
+        <>
+          <img
+            src={userData.profileImage || "/user.png"}
+            alt="User Profile"
+            className={styles.profileImage}
+          />
+          <div className={styles.userInfo}>
+            <span className={styles.userName}>
+              {userData.mentorName || "Unknown User"}
+            </span>
+            <span className={styles.userRole}>
+              {userData.role || "Unknown role"}
+            </span>
+          </div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };
