9e9d086eadc30d53f077df15e460bd24ed29e862
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-09-29 15:26:35 +0200

    Create applicationsHandler and  apiCalls

--- MT_Backend/services/jobs/handlers/applicationsHandler.js
+++ MT_Backend/services/jobs/handlers/applicationsHandler.js
@@ -1,6 +1,5 @@
 const Application = require("../../../pkg/jobs/applicationSchema");
 const Job = require("../../../pkg/jobs/jobSchema");
-
 const mongoose = require("mongoose");
 
 exports.createApplication = async (req, res, next) => {
@@ -15,6 +14,236 @@ exports.createApplication = async (req, res, next) => {
     const newApplication = await Application.create(req.body);
     res.status(201).json({
       application: newApplication,
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.updateApplication = async (req, res, next) => {
+  try {
+    if (req.body.jobId) {
+      const updatedJob = await Job.findByIdAndUpdate(req.body.jobId, {
+        status: "direct",
+      });
+    }
+
+    const updatedApplication = await Application.findByIdAndUpdate(
+      req.params.id,
+      req.body,
+      {
+        runValidators: true,
+        new: true,
+      }
+    );
+
+    if (!updatedApplication) {
+      const error = new Error("Job not found");
+      error.statusCode = 404;
+      return next(error);
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        application: updatedApplication,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.deleteApplication = async (req, res, next) => {
+  try {
+    if (req.body.mentorId) {
+      const deletedApplication = await Application.findOneAndDelete({
+        jobId: req.params.id,
+        mentorId: req.body.mentorId,
+      });
+      res.status(200).json({
+        status: "success",
+        data: {
+          application: deletedApplication,
+        },
+      });
+    }
+
+    const deletedApplication = await Application.findByIdAndDelete(
+      req.params.id
+    );
+
+    if (!deletedApplication) {
+      const error = new Error("Job not found");
+      error.statusCode = 404;
+      return next(error);
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        application: deletedApplication,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getAll = async (req, res, next) => {
+  try {
+    const application = await Application.find({});
+    res.status(200).json({
+      status: "success",
+      data: {
+        application: application,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getUserApplications = async (req, res, next) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const userId = req.params.id;
+    const acceptedStatus = req.query.acceptedStatus || false;
+
+    const limit = req.query.limit || 8;
+    const options = {
+      page: page,
+      limit: limit,
+      populate: {
+        path: "jobId",
+        select: "title",
+      },
+      sort: { createdAt: -1 },
+    };
+
+    if (acceptedStatus) {
+      const jobs = await Application.paginate(
+        {
+          mentorId: userId,
+          $and: [
+            { acceptedStatus: { $ne: "pending" } },
+            { acceptedStatus: acceptedStatus },
+          ],
+        },
+        options
+      );
+
+      if (!jobs) {
+        const error = new Error("This User doesn't exist");
+        error.statusCode = 404;
+        next(error);
+      }
+
+      res.json({
+        data: {
+          jobs: jobs,
+        },
+      });
+    }
+
+    const jobs = await Application.paginate(
+      {
+        mentorId: userId,
+        $and: [{ acceptedStatus: { $ne: "pending" } }],
+      },
+      options
+    );
+
+    if (!jobs) {
+      const error = new Error("This User doesn't exist");
+      error.statusCode = 404;
+      next(error);
+    }
+
+    res.json({
+      data: {
+        jobs: jobs,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.getUserPendingApplications = async (req, res, next) => {
+  try {
+    const page = parseInt(req.query.page) || 1;
+    const userId = req.params.id;
+
+    const limit = req.query.limit || 8;
+    const options = {
+      page: page,
+      limit: limit,
+      populate: {
+        path: "jobId",
+        select: "title",
+      },
+      sort: { createdAt: -1 },
+    };
+
+    if (req.query.applicationType) {
+      const pending = await Application.paginate(
+        {
+          mentorId: userId,
+          acceptedStatus: "pending",
+          applicationType: req.query.applicationType,
+        },
+        options
+      );
+
+      return res.json({
+        data: {
+          pending: pending,
+        },
+      });
+    }
+
+    const pending = await Application.paginate(
+      { mentorId: userId, acceptedStatus: "pending" },
+      options
+    );
+
+    if (!pending) {
+      const error = new Error("This User doesn't exist");
+      error.statusCode = 404;
+      next(error);
+    }
+
+    res.json({
+      data: {
+        pending: pending,
+      },
+    });
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.deleteOffer = async (req, res, next) => {
+  try {
+    const deletedApplication = await Application.findByIdAndDelete(
+      req.params.id
+    );
+
+    const deletedJob = await Job.findByIdAndDelete(deletedApplication.jobId);
+
+    if (!deletedApplication) {
+      const error = new Error("Job not found");
+      error.statusCode = 404;
+      return next(error);
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        application: deletedApplication,
+        job: deletedJob,
+      },
     });
   } catch (err) {
     next(err);
--- MT_Backend/services/jobs/index.js
+++ MT_Backend/services/jobs/index.js
@@ -11,10 +11,29 @@ const app = express();
 db.init();
 app.use(express.json());
 
-app.get("/api/v1/jobs/", jobs.getOpen);
-app.post("api/v1/applications", applications.createApplication);
+app.post("/api/v1/jobs", jobs.createJob);
+app.get("/api/v1/jobs", jobs.getOpen);
+app.get("/api/v1/jobs/:id", jobs.getJobById);
+app.put("/api/v1/jobs/:id", jobs.updateJob);
+app.delete("/api/v1/jobs/:id", jobs.deleteJob);
+app.post("/api/v1/jobs/offer", jobs.offerJob);
+app.get("/api/v1/jobs/company/:id", jobs.getJob);
 
-app.listen(process.env.PORT_USERS, (err) => {
+app.post("/api/v1/applications", applications.createApplication);
+app.get(
+  "/api/v1/applications/mentor/:mentorId",
+  applications.getUserApplications
+);
+app.get("/api/v1/applications", applications.getAll);
+app.put("/api/v1/applications/:id", applications.updateApplication);
+app.delete("/api/v1/applications/:id", applications.deleteApplication);
+app.get(
+  "/api/v1/applications/mentor/:id/pending",
+  applications.getUserPendingApplications
+);
+app.delete("/api/v1/applications/offer/:id", applications.deleteOffer);
+
+app.listen(process.env.PORT_JOBS, (err) => {
   if (err) {
     console.error("Service could not be initiated");
   }
