bc71ad29f9810838de93ffbfa394eff63f049f9e
Demjan Anatoli Golubov <demjan.a.golubov@gmail.com>
2024-09-29 16:47:50 +0200

    Rewrite component to show the navbar and not hide it everywhere

--- MentorToken/src/components/show/show.jsx
+++ MentorToken/src/components/show/show.jsx
@@ -1,33 +1,19 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, matchPath } from "react-router-dom";
+import { useLocation } from "react-router-dom";
 
 export const Show = ({ children }) => {
   const location = useLocation();
-  const [showNavBar, setShowNavBar] = useState(true);
+  const [showNavBar, setShowNavBar] = useState(false);
 
   useEffect(() => {
-    const hideNavBarHere = [
-      "/login",
-      "/register",
-      "/register/startup",
-      "/register/mentor",
-      "/test",
-      "/dashboard/startup",
-      "/dashboard/mentor",
-    ];
+    const showNavBarOnTheseRoutes = ["/", "/home", "/about", "/contact"];
 
-    const shouldHideNavBar = hideNavBarHere.some((path) =>
-      matchPath({ path, end: path === "/test" }, location.pathname)
+    const shouldShowNavBar = showNavBarOnTheseRoutes.includes(
+      location.pathname
     );
 
-    // const dynamicRoutes = ["/dashboard/startup/", "/dashboard/mentor/"];
-
-    // const isDynamicRoute = dynamicRoutes.some((route) =>
-    //   matchPath({ path: route, end: false }, location.pathname)
-    // );
-
-    setShowNavBar(!shouldHideNavBar);
+    setShowNavBar(shouldShowNavBar);
   }, [location]);
 
-  return <div>{showNavBar && children}</div>;
+  return <div>{showNavBar ? children : null}</div>;
 };
